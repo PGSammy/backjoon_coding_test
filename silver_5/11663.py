09aca1fa8181272175dbeaa7a9f9ed7a3198ddb3
PGSammy <gaemanssi2@gmail.com>
2025-02-19 23:31:24 +0900

    ✨ 2025/02/19 코딩테스트 2문제 풀이

---
+++ silver_5/11663.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+def find_start(locations, target):
+    start, end = 0, N - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if locations[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
+
+def find_end(locations, target):
+    start, end = 0, N - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if locations[mid] <= target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
+
+N, M = map(int, input().split())
+
+locations = list(map(int, input().split()))
+
+lines = [list(map(int, input().split())) for _ in range(M)]
+
+locations.sort()
+
+for line in lines:
+    start, end = line
+    start_idx = find_start(locations, start)
+    end_idx = find_end(locations, end)
+
+    print(end_idx - start_idx)
